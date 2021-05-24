bb8c2d706b6d2061e4cba934e6b84028462d4467
JohnMoran <johnnymoran44@hotmail.com>
2021-05-24 15:05:38 +0200

    only call api if searchText has value to avoid first call being blank

--- src/context/PhotoContext.js
+++ src/context/PhotoContext.js
@@ -9,14 +9,15 @@ const PhotoContextProvider = (props) => {
 	const [searchText, setSearchText] = useState("");
 
 	useEffect(() => {
-		runSearch(searchText);
+		searchText && runSearch(searchText);
 	}, [searchText]);
 
 	const runSearch = (searchText) => {
 		axios
 			.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${searchText}&per_page=24&format=json&nojsoncallback=1`)
 			.then((response) => {
-				setImages(response.data.photos.photo);
+				console.log(response.data.photos);
+				response.data.photos.photo.length > 0 && setImages(response.data.photos.photo);
 				setLoading(false);
 			})
 			.catch((error) => {

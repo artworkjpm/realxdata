393d0ff69d817c136fa8c23e40c0ab4593f401cb
JohnMoran <johnnymoran44@hotmail.com>
2021-05-24 18:31:13 +0200

    performance challenge done, saving urls and saving data to context

--- src/context/PhotoContext.js
+++ src/context/PhotoContext.js
@@ -7,24 +7,38 @@ const PhotoContextProvider = (props) => {
 	const [images, setImages] = useState([]);
 	const [loading, setLoading] = useState(true);
 	const [searchText, setSearchText] = useState("");
+	const [savedUrls, setSavedUrls] = useState([]);
+	const [savedViewedData, setSavedViewedData] = useState([{ url: "", data: [] }]);
 
 	useEffect(() => {
-		searchText && runSearch(searchText);
-	}, [searchText]);
+		const runSearch = (searchText) => {
+			axios
+				.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${searchText}&per_page=24&format=json&nojsoncallback=1`)
+				.then((response) => {
+					setImages(response.data.photos.photo);
+					setLoading(false);
+					setSavedUrls([...savedUrls, searchText]);
+					setSavedViewedData([...savedViewedData, { url: searchText, data: response.data.photos.photo }]);
+				})
+				.catch((error) => {
+					console.log("Encountered an error with fetching and parsing data", error);
+				});
+		};
 
-	const runSearch = (searchText) => {
-		axios
-			.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${searchText}&per_page=24&format=json&nojsoncallback=1`)
-			.then((response) => {
-				console.log(response.data.photos);
-				response.data.photos.photo.length > 0 && setImages(response.data.photos.photo);
-				setLoading(false);
-			})
-			.catch((error) => {
-				console.log("Encountered an error with fetching and parsing data", error);
-			});
-	};
-	return <PhotoContext.Provider value={{ images, loading, searchText, setSearchText }}>{props.children}</PhotoContext.Provider>;
+		if (searchText) {
+			if (savedUrls.indexOf(searchText) === -1) {
+				runSearch(searchText);
+			} else {
+				let result = savedViewedData.filter((o) => o.url.toLowerCase() === searchText.toLowerCase());
+				if (result.length > 0) {
+					setImages(result[0].data);
+					setLoading(false);
+				}
+			}
+		}
+	}, [searchText, savedUrls, savedViewedData]);
+
+	return <PhotoContext.Provider value={{ images, loading, searchText, setSearchText, savedUrls, savedViewedData }}>{props.children}</PhotoContext.Provider>;
 };
 
 export default PhotoContextProvider;
